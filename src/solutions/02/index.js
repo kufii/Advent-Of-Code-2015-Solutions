40ccc7a7f1413b2260e6d431857ecd09b6502c46
Adrien Pyke <adrien.pyke@034gc.onmicrosoft.com>
2019-12-02 13:45:31 -0500

    day 2

---
+++ src/solutions/02/index.js
@@ -0,0 +1,24 @@
+import input from './input';
+
+const getInput = () => input.split('\n').map(str => str.split('x').map(Number));
+
+const getSides = ([l, w, h]) => [l * w, w * h, h * l];
+
+const sum = (a, b) => a + b;
+
+const sortNum = (a, b) => a - b;
+
+export default {
+  part1: () =>
+    'Wrapping paper needed: ' +
+    getInput()
+      .map(getSides)
+      .map(sides => sides.reduce((a, b) => a + 2 * b, 0) + Math.min(...sides))
+      .reduce(sum),
+  part2: () =>
+    'Ribbon needed: ' +
+    getInput()
+      .map(dims => dims.sort(sortNum))
+      .map(([dim1, dim2, dim3]) => dim1 * 2 + dim2 * 2 + dim1 * dim2 * dim3)
+      .reduce(sum)
+};
