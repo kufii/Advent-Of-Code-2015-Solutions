81d32a0badaa335ffca423b854188a155d718424
Adrien Pyke <adrien.pyke@034gc.onmicrosoft.com>
2019-12-02 17:26:48 -0500

    broken day 8

--- src/solutions/08/index.js
+++ src/solutions/08/index.js
@@ -1,7 +1,25 @@
-const fetchInput = fetch('./input.txt')
-  .then(r => r.text())
-  .then(console.log);
+const fetchInput = () =>
+  fetch(
+    'https://raw.githubusercontent.com/kufii/Advent-Of-Code-2015-Solutions/master/src/solutions/08/input.txt'
+  ).then(r => r.text());
 
-fetchInput();
+fetchInput().then(console.log);
 
-export default {};
+export default {
+  part1: () =>
+    fetchInput().then(
+      str =>
+        str.replace(/\n/gu, '').length -
+        str
+          .split('\n')
+          .map(
+            str =>
+              str
+                .slice(1, str.length - 1)
+                .replace(/(?<!\\)(?:(\\\\)*)\\"/gu, '"')
+                .replace(/(?<!\\)(?:(\\\\)*)\\x[0-9a-f]"/giu, 'x')
+                .replace(/(?<!\\)(?:(\\\\)*)\\\\"/giu, '\\').length
+          )
+          .reduce((a, b) => a + b)
+    )
+};
