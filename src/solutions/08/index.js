3e3631eb683d95caefc1e684e7775eaf4de27f1a
Adrien Pyke <adpyke@gmail.com>
2019-12-03 00:54:54 -0500

    day 8

--- src/solutions/08/index.js
+++ src/solutions/08/index.js
@@ -1,25 +1,64 @@
+import dedent from 'dedent';
+import { sum } from '../../util';
+
 const fetchInput = () =>
+  localStorage.getItem('day8.input') ||
   fetch(
     'https://raw.githubusercontent.com/kufii/Advent-Of-Code-2015-Solutions/master/src/solutions/08/input.txt'
-  ).then(r => r.text());
+  )
+    .then(r => r.text())
+    .then(text => text.trim())
+    .then(text => (localStorage.setItem('day8.input', text), text));
 
-fetchInput().then(console.log);
+const escapeRegex = '((?<!\\\\)(?:(\\\\\\\\)*))';
+
+const decodeStr = str =>
+  str
+    .slice(1, str.length - 1)
+    .replace(new RegExp(`${escapeRegex}\\\\x[0-9a-f]{2}`, 'giu'), match =>
+      match.replace(
+        /\\x[0-9a-f]+/iu,
+        String.fromCharCode(parseInt(match.slice(match.length - 2), 16))
+      )
+    )
+    .replace(new RegExp(`${escapeRegex}\\\\"`, 'gu'), '$1"')
+    .replace(/\\\\/gu, '\\');
+
+const encodeStr = str => JSON.stringify(str);
+
+const getOriginalLength = file => file.replace(/\n/gu, '').length;
 
 export default {
   part1: () =>
-    fetchInput().then(
-      str =>
-        str.replace(/\n/gu, '').length -
+    Promise.resolve(fetchInput())
+      .then(str => [
+        getOriginalLength(str),
         str
           .split('\n')
-          .map(
-            str =>
-              str
-                .slice(1, str.length - 1)
-                .replace(/(?<!\\)(?:(\\\\)*)\\"/gu, '"')
-                .replace(/(?<!\\)(?:(\\\\)*)\\x[0-9a-f]"/giu, 'x')
-                .replace(/(?<!\\)(?:(\\\\)*)\\\\"/giu, '\\').length
-          )
-          .reduce((a, b) => a + b)
-    )
+          .map(str => decodeStr(str).length)
+          .reduce(sum)
+      ])
+      .then(
+        ([codeLength, memoryLength]) => dedent`
+          Code length: ${codeLength}
+          Memory length: ${memoryLength}
+          Result: ${codeLength - memoryLength}
+        `
+      ),
+  part2: () =>
+    Promise.resolve(fetchInput())
+      .then(str => [
+        getOriginalLength(str),
+        str
+          .split('\n')
+          .map(str => encodeStr(str).length)
+          .reduce(sum)
+      ])
+      .then(
+        ([codeLength, encodeLength]) => dedent`
+          Code length: ${codeLength}
+          Encoded length: ${encodeLength}
+          Result: ${encodeLength - codeLength}
+        `
+      )
 };
